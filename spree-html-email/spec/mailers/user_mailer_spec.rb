8845d1675fc7f4ed87110b5af27cd1c241ae34f4
Artyom Lagun <artyom.lagun@gmail.com>
2015-02-17 19:44:07 +0300

    add upgrade user_mailer test

--- spree-html-email/spec/mailers/user_mailer_spec.rb
+++ spree-html-email/spec/mailers/user_mailer_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 # require 'spree_auth_devise'
 
 describe 'Spree::UserMailer' do
+  let!(:store) { create(:store) }
   let(:user) { FactoryGirl.create(:user) }
 
   context "reset password instructions email" do
-    let(:email) { Spree::UserMailer.reset_password_instructions(user) }
+    let(:email) { Spree::UserMailer.reset_password_instructions(user, user.reset_password_token) }
 
     specify { email.content_type.should match("text/html") }
   end
