aebb211f982147e455c4c43c7053612744729877
fesstor <emelya1993@rambler.ru>
2015-04-03 12:50:16 +0300

    Fixed removing inventory items. 21 specs of 1880 failing.

--- spree_decimal_quantity/app/models/order_inventory_decorator.rb
+++ spree_decimal_quantity/app/models/order_inventory_decorator.rb
@@ -1,4 +1,22 @@
 Spree::OrderInventory.class_eval do
+
+  def verify(shipment = nil)
+    if order.completed? || shipment.present?
+
+      inventory_units_quantity = inventory_units.sum(:quantity)
+      if inventory_units_quantity < line_item.quantity
+        quantity = line_item.quantity - inventory_units_quantity
+
+        shipment = determine_target_shipment unless shipment
+        add_to_shipment(shipment, quantity)
+      elsif inventory_units_quantity > line_item.quantity
+        remove(inventory_units, shipment)
+      end
+    end
+  end
+
+  private
+
   def add_to_shipment(shipment, quantity)
     if variant.should_track_inventory?
       on_hand, back_order = shipment.stock_location.fill_status(variant, quantity)
@@ -17,18 +35,49 @@ Spree::OrderInventory.class_eval do
     quantity
   end
 
-  def verify(shipment = nil)
-    if order.completed? || shipment.present?
+  def remove(item_units, shipment = nil)
+    quantity = item_units.sum(:quantity) - line_item.quantity
 
-      inventory_units_quantity = inventory_units.sum(:quantity)
-      if inventory_units_quantity < line_item.quantity
-        quantity = line_item.quantity - inventory_units_quantity
-
-        shipment = determine_target_shipment unless shipment
-        add_to_shipment(shipment, quantity)
-      elsif inventory_units_quantity > line_item.quantity
-        remove(inventory_units, shipment)
+    if shipment.present?
+      remove_from_shipment(shipment, quantity)
+    else
+      order.shipments.each do |shipment|
+        break if quantity == 0
+        quantity -= remove_from_shipment(shipment, quantity)
       end
     end
+  end
+
+  def remove_from_shipment(shipment, quantity)
+    return 0 if quantity == 0 || shipment.shipped?
+
+    shipment_units = shipment.inventory_units_for_item(line_item, variant).reject do |variant_unit|
+      variant_unit.state == 'shipped'
+    end.sort_by(&:state)
+
+    quantity_to_remove = quantity
+
+    shipment_units.each do |inventory_unit|
+      break if quantity_to_remove == 0
+
+      if inventory_unit.quantity > quantity_to_remove
+        inventory_unit.quantity -= quantity_to_remove
+        inventory_unit.save!
+        quantity_to_remove = 0
+      else
+        quantity_to_remove -= inventory_unit.quantity
+        inventory_unit.destroy!
+      end
+    end
+    removed_quantity = quantity - quantity_to_remove
+
+    shipment.destroy if shipment.inventory_units.count == 0
+
+    # removing this from shipment, and adding to stock_location
+    if order.completed?
+      shipment.stock_location.restock variant, removed_quantity, shipment
+    end
+
+    removed_quantity
   end
 end
