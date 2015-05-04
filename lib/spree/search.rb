43a9d662c6664081ca836db1e56ea99eb64d0fc4
Peter Berkenbosch <peter@pero-ict.nl>
2015-05-05 00:26:25 +0200

    don't display brands or categories filter if they are already in the filter using the correct display for filters and selected values

--- lib/spree/search.rb
+++ lib/spree/search.rb
@@ -20,12 +20,31 @@ class Search
     end
   end
 
-  def facet_selected(facet)
+  def display_facet?(facet)
+    return false if facet == "prices"
+    return false if facet == "color"
+    return false if facet == "brands" && facet_selected(facet)
+    return false if facet == "categories" && facet_selected(facet)
+    true
+  end
 
+  def facet_and_value_selected(facet, value)
+    @properties[:facets].each do |f|
+      return true if f[:facet] == facet && f[:value] == value
+    end
+    return false
+  end
+
+  def facet_selected(facet)
+    @properties[:facets].each do |f|
+      return true if f[:facet] == facet
+    end
+    false
   end
 
   protected
   def setup_search(params)
+    @properties[:facets] = []
     @properties[:keywords] = params[:keywords] ||= ''
     filters = []
     if params[:filters].present?
@@ -34,7 +53,7 @@ class Search
         facet = param_filters[index]
         val = param_filters[index+1]
         filters << ("taxons.#{facet}:#{val}")
-        @properties[:facets] = {facet: facet, value: val }
+        @properties[:facets].push({ facet: facet, value: val })
       end
     end
     @properties[:filters] = filters
