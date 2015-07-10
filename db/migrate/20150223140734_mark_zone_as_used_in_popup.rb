2bd2e9e485d0476e511a2721ee8732643c612f9e
Peter Berkenbosch <peter@pero-ict.nl>
2015-07-10 11:23:11 +0200

    quick hack

--- db/migrate/20150223140734_mark_zone_as_used_in_popup.rb
+++ db/migrate/20150223140734_mark_zone_as_used_in_popup.rb
@@ -1,6 +1,6 @@
 class MarkZoneAsUsedInPopup < ActiveRecord::Migration
   def change
-    add_column :spree_zones, :for_popup, :boolean, default: false
-
+    #add_column :spree_zones, :for_popup, :boolean, default: false
   end
+
 end
