973f28d8573ae247171d2dd9be06f035962dd6ef
Peter Berkenbosch <peter@pero-ict.nl>
2015-08-08 18:05:17 +0200

    show categories all the time to fix the navigation with sub cats.

--- lib/spree/search.rb
+++ lib/spree/search.rb
@@ -37,7 +37,7 @@ class Search
     return false if facet == "prices"
     return false if facet == "colors"
     return false if facet == "brands"
-    return false if facet == "categories" && facet_selected(facet)
+    #return false if facet == "categories" && facet_selected(facet)
     true
   end
 
