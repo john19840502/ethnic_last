b4ab50b854feb314055fefe59591f808a37b89de
Peter Berkenbosch <peter@pero-ict.nl>
2015-10-05 14:10:54 +0200

    destroy 0 qty line_items on update

--- app/models/order_contents_decorator.rb
+++ app/models/order_contents_decorator.rb
@@ -1,10 +1,8 @@
 Spree::OrderContents.class_eval do
 
   def update_cart(params)
-
     if order.update_attributes(filter_order_items(params))
       order.line_items = order.line_items.select { |li| li.quantity > 0 }
-      order.save
       # Update totals, then check if the order is eligible for any cart promotions.
       # If we do not update first, then the item total will be wrong and ItemTotal
       # promotion rules would not be triggered.
@@ -14,6 +12,8 @@ Spree::OrderContents.class_eval do
       persist_totals
       true
     else
+      order.line_items.select { |li| li.quantity == 0 }.collect(&:destroy)
+      order.update!
       false
     end
   end
