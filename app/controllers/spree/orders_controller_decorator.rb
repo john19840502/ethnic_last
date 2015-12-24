fb59fd6b7b722d9dc2b946fe1fd1d806af3e3d03
Kunal Chaudhari <kunalchaudhari@gmail.com>
2015-12-24 08:59:20 +0530

    fix bug when item quantity is too big

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,6 +1,32 @@
 Spree::OrdersController.class_eval do
   before_action :go_back_to
 
+  def update
+    begin
+      if @order.contents.update_cart(order_params)
+        respond_with(@order) do |format|
+          format.html do
+            if params.has_key?(:checkout)
+              @order.next if @order.cart?
+              redirect_to checkout_state_path(@order.checkout_steps.first) and return
+            else
+              redirect_to cart_path and return
+            end
+          end
+        end
+      end
+    rescue ActiveRecord::StatementInvalid
+      error = Spree.t(:please_enter_reasonable_quantity)
+    end
+
+    if error
+      flash[:error] = error
+      redirect_back_or_default(spree.cart_path)
+    else
+      respond_with(@order)
+    end
+  end
+
   def go_back_to
     if request.referer.present? && !request.referer.include?("cart")
       session[:go_back_to] = request.referer
