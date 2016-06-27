4e079c03be994511cb01e04bd9bd8c49ae11fc15
root <jlee021199@gmail.com>
2016-06-27 02:05:47 -0400

    update

---
+++ app/models/flat_percent_item_total_decorator.rb
@@ -0,0 +1,18 @@
+Spree::Calculator::FlatPercentItemTotal.class_eval do
+  def compute(object)
+
+    if object.tax_zone == Spree::Zone.default_tax
+      computed_amount  = (object.amount * preferred_flat_percent / 100).round(5)
+    else
+      computed_amount  = (object.pre_tax_amount * preferred_flat_percent / 100).round(5)
+    end
+    
+    # We don't want to cause the promotion adjustments to push the order into a negative total.
+    if computed_amount > object.amount
+      object.amount
+    else
+      computed_amount
+    end
+  end
+end
+
